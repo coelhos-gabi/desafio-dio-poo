15efa6aaf564a91567c643363143be55c377463c
coelhos-gabi <coelhos.gabi@gmail.com>
2022-07-28 10:38:42 -0300

    finalização projeto bootcamp

---
+++ README.md
@@ -0,0 +1,7 @@
+## DESAFIO PROGRAMAÇÃO ORIENTADA A OBJETOS
+
+1. Abstração de um domínio bootcamp e modelagem de seus atributos e métodos
+2. Criação das classes: bootcamp, cursos, mentorias e devs e seus relacionamentos
+3. Classes cursos, mentoria e devs foram modeladas, ou seja, foram criados seus atributos e métodos
+4. Uso de paradigmas de orientação a objetos: abstração, encapsulamento, herança e polimorfismo
+5. Uso de objetos para representção das classes criadas
--- src/br/com/dioprojetos/Main.java
+++ src/br/com/dioprojetos/Main.java
@@ -1,7 +1,65 @@
 package br.com.dioprojetos;
 
+import br.com.dioprojetos.desafio.dominio.*;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso java");
+        curso1.setDescricao("descrição curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso js");
+        curso2.setDescricao("descrição curso js");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("mentoria java");
+        mentoria1.setDescricao("descrição mentoria java");
+        mentoria1.setData(LocalDate.now().plusDays(15));
+
+        Mentoria mentoria2 = new Mentoria();
+        mentoria2.setTitulo("mentoria js");
+        mentoria2.setDescricao("descrição mentoria js");
+        mentoria2.setData(LocalDate.now().plusDays(15));
+
+//        System.out.println(curso1);
+//        System.out.println(curso2);
+//        System.out.println(mentoria1);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria1);
+        bootcamp.getConteudos().add(mentoria2);
+
+        Dev dev1 = new Dev();
+        dev1.setNome("Gabriela");
+        dev1.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos inscritos " + dev1.getNome() + " :\n" + dev1.getConteudosInscritos());
+        dev1.progredir();
+        dev1.progredir();
+        System.out.println("Conteudos concluídos " + dev1.getNome() + " :\n" + dev1.getConteudosConcluidos());
+        System.out.println("Conteudos inscritos " + dev1.getNome() + " :\n" + dev1.getConteudosInscritos());
+        System.out.println("XP: " + dev1.calcularTotalXp());
+
+        System.out.println("------------------------------");
+        Dev dev2 = new Dev();
+        dev2.setNome("João");
+        dev2.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos inscritos " + dev2.getNome() + " :\n"+ dev2.getConteudosInscritos());
+        dev2.progredir();
+        dev2.progredir();
+        dev2.progredir();
+        System.out.println("Conteudos concluídos " + dev2.getNome() + " :\n" + dev2.getConteudosConcluidos());
+        System.out.println("Conteudos inscritos " + dev2.getNome() + " :\n"+ dev2.getConteudosInscritos());
+        System.out.println("XP: " + dev2.calcularTotalXp());
+
+
     }
 }
